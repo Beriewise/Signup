93f50d7fb505f6d2fd3f3e852fabe733b186df74
Beriewise <berieremo@gmail.com>
2022-11-24 22:13:45 +0300

    https://github.com/Beriewise/jslessons.git

---
+++ homepage/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './Login';
+import Register from Register.js;
+
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login/>
+    </main>
+  );
+}
+
+export default App;
